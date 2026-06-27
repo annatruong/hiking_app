361a512ed5d69b6c6e40ba1bd819b816e63517bf
Anna Truong <annatruong321@hotmail.com>
2026-06-27 13:57:50 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,54 @@
+# Converts kilometres to metres
+def convert_km_to_m(kilometres):
+    return kilometres * 1000
+# Calculates the average gradient using ascent and distance. Returns value in 1 decimal place
+def get_average_gradient_percentage(ascent, distance):
+    return round((ascent / convert_km_to_m(distance)) * 100, 1)
+
+# Calculates ascent per kilometre
+def get_average_climb(ascent, distance):
+    return round(ascent / distance)
+
+# Get rating base of average climb (m/km)
+def get_rating(m_per_km):
+    if m_per_km < 20:
+        return "🟢 Easy"
+    elif 20 <= m_per_km < 40:
+        return "🟡 Moderate"
+    elif 40 <= m_per_km < 60:
+        return "🟠 Challenging"
+    elif 60 <= m_per_km < 80:
+        return "🔴 Hard"
+    else:
+        return "⚫️ Very Hard"
+
+def main():
+
+    print(r"""
+       /\    /\    /\    /\    /\    /\   
+      /  \  /  \  /  \  /  \  /  \  /  \  
+     /____\/____\/____\/____\/____\/____\  
+
+              🥾 HIKING APP 🥾
+     Gradient • Climb • Difficulty Rating
+        How steep is your adventure?
+    """)
+    # Get ascent and distance from user
+    ascent = int(input("Enter the total ascent(m): "))
+    distance = float(input("Enter the total distance(km): "))
+
+    # Calculate average gradient and average climb
+    average_gradient = get_average_gradient_percentage(ascent, distance)
+    average_climb = get_average_climb(ascent, distance)
+    rating = get_rating(average_climb)
+
+    # Display results
+    print("\n================ HIKE EVALUATION ================\n")
+    print(f"Distance: {distance} km")
+    print(f"Ascent: {ascent} m")
+    print(f"Average gradient: {average_gradient}%")
+    print(f"Average climb: {average_climb} m/km")
+    print(f"Difficulty: {rating}")
+    print("\n=================================================\n")
+
+main()
