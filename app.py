cd435168d0a24f13f130848cfe7ecef781eb84ff
Anna Truong <annatruong321@hotmail.com>
2026-07-18 12:15:51 +0100

    fixed form resubmission issue

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, url_for
 from config import Config
 from forms import HikeForm
 from calculation import get_hike_estimation
@@ -10,12 +10,27 @@ app.config.from_object(Config)
 @app.route("/", methods=["GET","POST"])
 def home():
     form = HikeForm()
+    
+    # if user submitted form, get/calculate data and store in session, then redirect to same page
     if form.validate_on_submit():
-        results = get_hike_estimation(form.ascent.data,form.distance.data)
+        session["ascent"] = form.ascent.data
+        session["distance"] = form.distance.data
+        session["results"] = get_hike_estimation(form.ascent.data,form.distance.data)
+        
+        return redirect(url_for("home"))
+    
+    results = session.pop("results", None)
+    ascent = session.pop("ascent", None)
+    distance = session.pop("distance", None)
+    form.ascent.data = ascent
+    form.distance.data = distance
+    
+    return render_template("index.html", 
+        form=form, 
+        ascent=ascent, 
+        distance=distance, 
+        results=results)
 
-        return render_template("index.html", form=form, ascent=form.ascent.data, distance=form.distance.data, results=results)
-
-    return render_template("index.html", form=form)
 
 
 if __name__ == "__main__":
