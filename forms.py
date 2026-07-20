dec584aadd9ca7bc6ede8bfd189e273f56a0611a
Anna Truong <annatruong321@hotmail.com>
2026-07-20 22:06:24 +0100

    added tailwind

--- forms.py
+++ forms.py
@@ -3,6 +3,6 @@ from wtforms import IntegerField, SubmitField
 from wtforms.validators import DataRequired, NumberRange 
 
 class HikeForm(FlaskForm):
-    ascent = IntegerField("Ascent (m)", validators=[DataRequired(), NumberRange(min=0)])
-    distance = IntegerField("Distance (km)",validators=[DataRequired(), NumberRange(min=1)])
+    ascent = IntegerField("Ascent (m):", validators=[DataRequired(), NumberRange(min=0)])
+    distance = IntegerField("Distance (km):",validators=[DataRequired(), NumberRange(min=1)])
     submit = SubmitField("Calculate")
